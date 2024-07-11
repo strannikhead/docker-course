4dc2bb936f43b6641e3cba2ac5fa9b8b30864ad4
PhonkX <nighthawk3371@gmail.com>
2024-07-11 14:33:06 +0500

    Initial commit

---
+++ WebApp/Data/DataExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApp.Data
+{
+    public static class DataExtensions
+    {
+        public static void PrepareDB(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var env = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+                    var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+                    if (env.IsDevelopment()
+                        && string.IsNullOrEmpty(configuration.GetConnectionString("SqlServerConnection")))
+                    {
+                        scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
+                    }
+                }
+                catch (Exception e)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occurred while migrating or seeding the database.");
+                }
+            }
+        }
+    }
+}
---
+++ WebApp/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(50)]
+        public override string Id { get; set; }
+    }
+}
